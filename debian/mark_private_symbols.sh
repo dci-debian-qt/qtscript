fe6bb603a3ab1aedf057ed806a0cad32ca0ebe8e
Dmitry Shachnev <mitya57@gmail.com>
2015-03-01 22:05:22 +0300

    mark_private_symbols.sh: Strip trailing colon from symbols names.

--- debian/mark_private_symbols.sh
+++ debian/mark_private_symbols.sh
@@ -54,6 +54,7 @@ grep -rh class ${PRIVATE_HEADERS} |
 	grep EXPORT | 
 	while read class export classname rest 
 	do
+		classname=$(echo $classname | sed 's/://')
 		echo ${#classname}${classname} 
 	done | 
 	while read privateclass 
